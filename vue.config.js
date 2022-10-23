45832ea06a0f487f97fa1c06a23a445566417009
bingqi <bingqi@guanghe.tv>
2022-10-23 19:29:52 +0800

    Initial the page of project

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,30 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  // 配置前端服务地址和端口
-  server: {
-    host: "0.0.0.0",
-    port: 3000,
-    // 是否开启 https
-    https: false,
+module.exports = {
+  //根据屏幕大小自动缩放适配
+  css: {
+      loaderOptions: {
+          css: {
+              // options here will be passed to css-loader
+          },
+          postcss: {
+              // options here will be passed to postcss-loader
+              plugins: [require('postcss-px2rem')({
+                  remUnit: 192,  //屏幕适配
+              })]
+          }
+      }
   },
-  base: ".",
-  publicPath:'.'
-});
+  
+  lintOnSave:false, //取消eslint检测
+  productionSourceMap: false,  //设为false可减小打包后包体积，默认为true
+  publicPath: '/cm/',  //配置域名根路径，从localhost/#/配置后成localhost/cm#/，注意路由应使用hash 
+  devServer: {
+      overlay:{
+          warning:false,
+          error:true
+      },
+      host: '0.0.0.0',
+      port: 8060,  
+      // open: true,  //自动打开浏览器
+  },
+  assetsDir: "static"  //设置打包后静态资源放在static文件夹里
+} 
